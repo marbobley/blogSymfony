ad6778a7e9e6373007bf4ebb9bacdd41a9c71d2c
Marbobley <fournier.wilf@gmail.com>
2026-02-07 11:25:18 +0100

    start security

---
+++ src/Infrastructure/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use App\Application\UseCaseInterface\ListPostsInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(ListPostsInterface $listPosts): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'posts' => $listPosts->execute(),
+        ]);
+    }
+}
