bd4044d90e8a2203b64b174d82a95700299972d7
Marbobley <fournier.wilf@gmail.com>
2026-03-01 09:22:20 +0100

    Update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,33 +4,61 @@ declare(strict_types=1);
 
 namespace App\DataFixtures;
 
-use App\Infrastructure\Entity\Post;use App\Infrastructure\Entity\Tag;use App\Infrastructure\Entity\User;use DateTimeImmutable;use Doctrine\Bundle\FixturesBundle\Fixture;use Doctrine\Persistence\ObjectManager;use Exception;use function array_rand;use function rand;use function str_repeat;
+use App\Domain\Service\PasswordBlogHasherInterface;
+use App\Infrastructure\Entity\Post;
+use App\Infrastructure\Entity\PostLike;
+use App\Infrastructure\Entity\Tag;
+use App\Infrastructure\Entity\User;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Exception;
+use function array_rand;
+use function rand;
+use function str_repeat;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly PasswordBlogHasherInterface $passwordHasher
+    )
+    {
+    }
+
     /**
      * @throws Exception
      */
     public function load(ObjectManager $manager): void
     {
-        $password = '$2y$13$D2lg.iFaKCmwbbbu3hKFhOeAyXgpL05Lp9TsDOSrH4XK57SS7x.z6';
-        // --- USERS ---
-        $admin = new User(); // En prod, on encoderait le mot de passe
-        $admin->setEmail('admin@blog.com');
-        $admin->setPassword($password);
-        $admin->setRoles(['ROLE_ADMIN']);
-        $manager->persist($admin);
+        $admin = $this->generateUser('admin@blog.com', ['ROLE_ADMIN']);
 
-        $user = new User();
-        $user->setEmail('user@blog.com');
-        $user->setPassword($password);
-        $manager->persist($user);
+        $user1 = $this->generateUser('user1@blog.com', ['ROLE_USER']);
+        $user2 = $this->generateUser('user2@blog.com', ['ROLE_USER']);
+        $user3 = $this->generateUser('user3@blog.com', ['ROLE_USER']);
+        $user4 = $this->generateUser('user4@blog.com', ['ROLE_USER']);
+        $user5 = $this->generateUser('user5@blog.com', ['ROLE_USER']);
+        $user6 = $this->generateUser('user6@blog.com', ['ROLE_USER']);
+        $user7 = $this->generateUser('user7@blog.com', ['ROLE_USER']);
+        $manager->persist($admin);
+        $manager->persist($user1);
+        $manager->persist($user2);
+        $manager->persist($user3);
+        $manager->persist($user4);
+        $manager->persist($user5);
+        $manager->persist($user6);
+        $manager->persist($user7);
 
         // --- TAGS ---
         $tags = $this->generateTags($manager);
 
         // --- POSTS ---
-        $this->generatePosts($tags, $manager);
+        $posts = $this->generatePosts($tags, $manager);
+
+        $like = new PostLike();
+        $like->setPost($posts[0]);
+        $like->setUser($user1);
+        $like->setCreatedAt(new DateTimeImmutable());
+        $manager->persist($like);
 
         $manager->flush();
     }
@@ -54,18 +82,19 @@ class AppFixtures extends Fixture
     /**
      * @param Tag[] $tags
      * @param ObjectManager $manager
-     * @return void
+     * @return Post[]
      * @throws Exception
      */
-    public function generatePosts(array $tags, ObjectManager $manager): void
+    public function generatePosts(array $tags, ObjectManager $manager): array
     {
+        $posts = [];
         for ($i = 1; $i <= 10; $i++) {
             $post = new Post(
-                "Article numéro {$i}",
-                "Ceci est le contenu de l'article numéro {$i}. Il contient des informations très intéressantes sur le développement Symfony. "
+                "Article numéro $i",
+                "Ceci est le contenu de l'article numéro $i. Il contient des informations très intéressantes sur le développement Symfony. "
                     . str_repeat(string: 'Lorem ipsum dolor sit amet. ', times: 5),
             );
-            $post->setSubTitle("Ceci est le soutitre {$i}");
+            $post->setSubTitle("Ceci est le soutitre $i");
             $post->publish();
 
             $createdAt = new DateTimeImmutable('-' . (11 - $i) . ' days');
@@ -79,9 +108,21 @@ class AppFixtures extends Fixture
             foreach ($randomTags as $index) {
                 $tag = $tags[$index];
                 $post->addTag($tag);
+                $posts[] = $post;
             }
-
             $manager->persist($post);
+
         }
+        return $posts;
+    }
+
+    private function generateUser(string $email, array $roles) : User
+    {
+        $password = $this->passwordHasher->hash($email, 'password');
+        $user = new User(); // En prod, on encoderait le mot de passe
+        $user->setEmail($email);
+        $user->setPassword($password);
+        $user->setRoles($roles);
+        return $user;
     }
 }
